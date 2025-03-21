6bc43e50655da01d9a8c644d9a444930b556ebb1
patansharukhan <patansharu654321@gmail.com>
2025-03-21 17:33:07 +0530

    added authentication - wip

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,25 @@
-import { NextFunction, Request, Response } from "express";
-
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+import User from '../models/user.model';
+const router = Router();
 
 /* GET home page. */
-router.get('/', function (req: Request, res: Response, next: NextFunction) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    if (user.password === password) {
+      const jwtToken = 'jwt-token'
+      res.status(200).json(jwtToken)
+    } else {
+      res.status(422).json('Invalid Password')
+    }
+  } else {
+    res.status(422).json('Invalid Credentials')
+  }
+})
 
 module.exports = router;
